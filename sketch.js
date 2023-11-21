c3ebb743d1573e9a286c7c51878e6e9b940fc6bd
novicecodes <151675721+novicecodes@users.noreply.github.com>
2023-11-21 22:04:29 +0000

    Update

--- sketch.js
+++ sketch.js
@@ -1,24 +1,18 @@
-let juaFont;
-
-function preload() {
-  // Load the Jua font
-  juaFont = loadFont('https://fonts.googleapis.com/css2?family=Jua&display=swap');
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(255, 206, 91);
-  
-  textAlign(CENTER, CENTER);
-  textSize((width + height) / 32);
+background(255, 206, 91);
 
-  // Set the font to Jua
-  textFont(juaFont);
+textAlign(CENTER, CENTER);
+textSize((width + height) / 32);
 
-  text("tabby.cat", width / 2, height / 2 - 12);
-  textSize((width + height) / 48);
-  text("coming soon!!!", width / 2, height / 2 + 24);
+// Set the font to Jua
+textFont('Jua');
+
+text("tabby.cat", width / 2, height / 2 - 12);
+textSize((width + height) / 48);
+text("coming soon!!!", width / 2, height / 2 + 24);
 }
+  
